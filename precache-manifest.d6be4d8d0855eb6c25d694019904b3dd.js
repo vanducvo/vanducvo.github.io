11c41b7f97ec5d85be51327e3d77d5596c703b3a
Van Duc Vo <vanducvo@gmail.com>
2020-05-02 16:27:31 +0700

    Updates

---
+++ precache-manifest.d6be4d8d0855eb6c25d694019904b3dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015028d656ba5924a03574ebcc01235e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe2eb298506a9a0b5058",
+    "url": "/static/css/main.9ad84e68.chunk.css"
+  },
+  {
+    "revision": "a21796c4d323d431e0cf",
+    "url": "/static/js/2.e88ad10f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e88ad10f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2eb298506a9a0b5058",
+    "url": "/static/js/main.b648b330.chunk.js"
+  },
+  {
+    "revision": "75c2f5593f68c56675f5",
+    "url": "/static/js/runtime-main.c2a23a7a.js"
+  },
+  {
+    "revision": "61bc39bfbe0564387277750859bfaf72",
+    "url": "/static/media/expand.61bc39bf.svg"
+  },
+  {
+    "revision": "7b2d76303644b66a97a9b82e1c20f38e",
+    "url": "/static/media/logo.7b2d7630.svg"
+  },
+  {
+    "revision": "e2eaa5f55300e19eeacd16d5596c5906",
+    "url": "/static/media/profile.e2eaa5f5.jpg"
+  }
+]);
